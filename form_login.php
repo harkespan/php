73e6d774589946cdd93bc8f8c18cba3b14afc22e
octone.harry@gmail.com <octone.harry@gmail.com>
2022-04-01 08:48:53 +0000

    delete files

--- form_login.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="ceklogin.php">
- Username : <input type="text" name="nim"/><br/>
-Password :  <input type="password" name="paswd">
-  <input type="submit" value="login">
-</form>
